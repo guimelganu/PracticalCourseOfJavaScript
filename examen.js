87228a39eedc0cb44ae1f4df9a4c2ab3bae575e8
JGARC964 <jgarc964@ford.com>
2023-07-24 10:10:34 -0600

    Initial Commit

---
+++ examen.js
@@ -0,0 +1,11 @@
+
+let name = "Jorge";
+let lastname = "Garcia";
+let platziUserName = "GuimelGaNu"
+let age = 26;
+let email = "guimelgn96@hotmail.com";
+let mayorDeEdad = 18;
+let savedMoney = 20000;
+let spents = 15000;
+
+console.log(name + " " + lastname);
