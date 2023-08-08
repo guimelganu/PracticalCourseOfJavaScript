097605db4e26ba457809d7235571a138a520b25e
guimelganu <guimelgn96@hotmail.com>
2023-08-08 00:35:35 -0600

    Final Exam Changes

--- examen.js
+++ examen.js
@@ -1,18 +1,18 @@
 
-// let name = "Jorge";
-// let lastname = "Garcia";
-// let platziUserName = "GuimelGaNu"
-// let age = 26;
-// let email = "guimelgn96@hotmail.com";
-// let mayorDeEdad = 18;
-// let savedMoney = 20000;
-// let spents = 15000;
+let name = "Jorge";
+let lastname = "Garcia";
+let platziUserName = "GuimelGaNu"
+let age = 26;
+let email = "guimelgn96@hotmail.com";
+let mayorDeEdad = 18;
+let savedMoney = 20000;
+let spents = 15000;
 
-// let realMoney = savedMoney - spents
+let realMoney = savedMoney - spents
 
-// console.log("Nombre Completo: " + name + " " + lastname);
-// console.log("Dinero Real: " + (savedMoney-spents));
-// console.log(`Real Money: ${spents}`);
+console.log("Nombre Completo: " + name + " " + lastname);
+console.log("Dinero Real: " + (savedMoney-spents));
+console.log(`Real Money: ${spents}`);
 
 
 function saludar (name, lastname, nickname){
@@ -39,8 +39,6 @@ else{
     console.log("Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año.");
 }
 
-
-
 if(tipoDeSuscripcion == "Free") {
     console.log("Solo puedes tomar los cursos gratis.");
 }
@@ -55,6 +53,24 @@ if(tipoDeSuscripcion == "ExpertPlus"){
 }
 
 
+let tiposDeSuscripcion = {
+    free: "Solo puedes tomar los cursos gratis.",
+    basic: "Puedes tomar casi todos los cursos de Platzi durante un mes.",
+    expert: "Puedes tomar casi todos los cursos de Platzi durante un año",
+    expertDuo: "Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año."
+}
+
+function determinarTipoDeSuscripcion(suscripcion){
+    if(tiposDeSuscripcion[suscripcion]){
+        console.log(tiposDeSuscripcion[suscripcion])
+        return;
+    }
+    console.warn("Ese tipo de suscripción no sirve");
+}
+
+determinarTipoDeSuscripcion("expertDuo");
+
+
 let x = 0;
 while(x  < 5){
     console.log(`Hola Perro por ${x} vez`);
@@ -67,10 +83,9 @@ while(y>=2){
     y--;
 }
 
-// do{
-//     var response = prompt("Cuanto es 2 + 2?");
-// }while(response != 2);
-
+do{
+    var response = prompt("Cuanto es 2 + 2?");
+}while(response != 2);
 
 
 function receiveArray (array = []){
@@ -89,19 +104,43 @@ function receiveArrayOneByOne (array2 = []) {
 receiveArrayOneByOne(["Manzana","Pera","Jitomate"]);
 
 
-var carros = [
+let carros = [
     {nombre: "Cadillac", año: "1720"},
     {nombre: "Mac-E", año: "2023"},
     {nombre: "Mustang", año: "2018"}
 ];
 
+let persona = {
+    nombre: "Pepe",
+    edad: 32,
+    nickname: "Pepito"
+}
 
 function receiveObjects (carros) {
     return carros.forEach(carro => {
-        console.log(carro.año)
+        console.log(carro)
     }); 
 }
 
-
+function imprimirObjeto (obj) {
+    for(var i in obj){
+        console.log(persona[i]);
+    }
+}
 
 receiveObjects(carros);
+
+imprimirObjeto(persona);
+
+function printNumbers() {
+	for (var i = 0; i < 10; i++) {
+		setTimeout(
+        function printer() {
+            console.log(i);
+            },
+			100 * i
+		);
+	}
+}
+
+printNumbers();
